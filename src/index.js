ffa53e881282ccf8f26b4fe1225618b66c614f51
Kemet Dugue <kemet.dugue@gmail.com>
2018-08-07 16:23:45 -0400

    add map functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+const pKey = require('../secrets.js')
+
+mapboxgl.accessToken = pKey.key;
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: 'mapbox://styles/mapbox/streets-v10'
+});
+
 console.log("index.js running");
